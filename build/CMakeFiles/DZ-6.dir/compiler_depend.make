d2fd13eecaba941124980e74d8c96c2af5247df5
Igor Rogozin <827779@gmail.com>
2023-05-25 11:16:11 +0300

    CMakeList

---
+++ build/CMakeFiles/DZ-6.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for DZ-6.
+# This may be replaced when dependencies are built.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/igorrogozin/Documents/ProjectsCPP/DZ-6
+# Build directory: /Users/igorrogozin/Documents/ProjectsCPP/DZ-6/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
