85bd05aafe23da2489da45ee1522b311b8c4a28c
Naqoyqatsii <65499757+Naqoyqatsii@users.noreply.github.com>
2023-05-25 11:17:19 +0300

    Delete build directory

--- build/CMakeFiles/DZ-6.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for DZ-6.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/igorrogozin/Documents/ProjectsCPP/DZ-6
-# Build directory: /Users/igorrogozin/Documents/ProjectsCPP/DZ-6/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
